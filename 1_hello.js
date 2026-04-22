2d49cea58aca9200e9ac7b7dab32e2ca65ad6837
Suxumuxu <emmanouil.xylakis@gmail.com>
2026-04-22 11:07:48 +0300

    Intro to JS

---
+++ 1_hello.js
@@ -0,0 +1,26 @@
+// console.log("Hello from hello.js!");
+
+// let age = 38;
+// let height = 1.75;
+// let student = "John Doe";
+// let isStudent = true;
+// let diameter = 3;
+
+// const PI = 3.14159;
+
+// student = "Manos Xylakis";
+
+// circumference = diameter * PI ;
+
+// console.log(age);
+// age = 45; 
+// console.log(age);
+// console.log(height);
+// console.log(student);
+// console.log(isStudent);
+
+// console.log("Hello " + student + "\nYou are " + age + " years old and " + height + " meters tall");
+
+// alert("Hello " + student + " You are " + age + " years old and " + height + " meters tall and my name " +
+//     "is" + student);
+
---
+++ 2_compound_data.js
@@ -0,0 +1,105 @@
+let zygoi = [0,2,4,6,8]
+let monoi = [1,3,5,7,9]
+let unsorted = [5,2,9,1,5,6]
+
+// console.log(zygoi[2])
+// console.log(monoi[1])
+
+let matrix = [
+    ["X","O","x"],
+    ["O","O","O"],
+    ["X","O","x"]
+]
+// alert(matrix[0][1])
+// console.log(matrix.length)
+// console.log(structuredClone(matrix));
+matrix[0][1] = "X";
+
+// alert(matrix[0][1])
+// console.log(matrix)
+// console.log(structuredClone(matrix));
+
+// console.log(typeof(isStudent))
+// console.log(typeof(height))
+
+// remove by selection of value
+zygoi = zygoi.filter(x => x !== 20);
+
+// add element to the end of the list 
+zygoi.push(12);
+
+// add element to the beginning of the list 
+zygoi.unshift(8);
+
+// FIFO and LIFO 
+zygoi.pop();    // removes last
+zygoi.shift();  // removes first
+
+//using a clone to keep my initial list intakt 
+let unsorted_bak = structuredClone(unsorted)
+
+let sortedList = unsorted.sort()
+// console.log(sortedList)
+// console.log(unsorted_bak)
+
+unsorted.reverse()
+// console.log(unsorted)
+
+let mylist = unsorted.slice(1,-2)
+// console.log(mylist)
+// console.log(unsorted)
+
+// console.log(zygoi)
+
+//movie object
+let casablanca = {
+"title": "Casablanca",
+"released": 1942,
+"director": "Michael Curtiz",
+"awards": ['best_director', 'best_movie', 'best_soundtrack'],
+"main_protagonist" : "Mathew Perry",
+"crew" : ['peter', 'georgia', 'helen']
+}
+
+//pet object
+
+//accessing object value using dot notation
+// console.log(casablanca.title)
+
+// console.log(casablanca['title'])
+
+//accessing object value using array notation
+// console.log(casablanca['crew'])
+
+casablanca.released = 1941
+
+// console.log(casablanca)
+
+let pet = {};
+
+pet.name = "Fluffy";
+pet.age = 3;
+pet.type = 'guinea pig';
+pet.favorite_foods = ['lettuce', 'carrots', 'cucumbers ']
+
+let myOtherPet = {
+    name : "Viktor",
+    age : 5,
+    type : "dog",
+    favorite_foods : ['steak', 'sausages', 'lamb chops', 'dog food']
+}
+
+let my_pets = [pet,myOtherPet]
+
+// make a new holder variable
+const fs = require('fs');
+
+// Convert object → JSON string
+const json = JSON.stringify(my_pets, null, 2);
+
+// Save to file
+fs.writeFileSync('data/my_pets.json', json);
+
+console.log(my_pets);
+
+console.log(JSON.stringify(my_pets))
