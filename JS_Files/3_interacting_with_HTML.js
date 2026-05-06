e9d50f91024051a54ca8e2035fbafe9ba5600e9a
Suxumuxu <emmanouil.xylakis@gmail.com>
2026-05-06 11:40:31 +0300

    events and mouse events

--- JS_Files/3_interacting_with_HTML.js
+++ JS_Files/3_interacting_with_HTML.js
@@ -1,14 +1,16 @@
 const heading = document.getElementById("top-heading");
 const paragraph = document.getElementById("main-paragraph");
-const headingSecond = document.getElementById("second-heading")
+const headingSecond = document.getElementById("second-heading");
+const headings = document.querySelectorAll('h1, h2');
 
-console.log(heading.innerText);
-console.log(paragraph.innerText);
+
+// console.log(heading.innerText);
+// console.log(paragraph.innerText);
 
 headingSecond.innerText = "I have changed the Text for my Second Heading!"
 
-let highlightElements = document.querySelectorAll('.highlight')
+const highlightElements = document.querySelectorAll('.highlight')
 
-highlightElements.forEach(el => {
-  el.style.backgroundColor = "blue";
-});
+// highlightElements.forEach(el => {
+//   el.style.backgroundColor = "blue";
+// });
