19f1185e42ee73a3635c0a33aba6a6ee11fde043
Suxumuxu <emmanouil.xylakis@gmail.com>
2026-05-13 08:51:47 +0300

    JS EVENTS

--- JS_Files/4_eventHandlers.js
+++ JS_Files/4_eventHandlers.js
@@ -1,7 +1,6 @@
 const clrbtn = document.getElementById("clrBtn");
 const increaseFontBtn = document.getElementById("increaseFontBtn");
 const decreaseFontBtn = document.getElementById("decreaseFontBtn");
-
 let paragraphMouse = document.getElementById("paragraph-mouse")
 
 heading.addEventListener("click", () => {
@@ -18,28 +17,22 @@ clrbtn.addEventListener("click", ()=>{
     
     const colors = ["blue","red","pink","yellow","green","cyan"];
     const color = colors[Math.floor(Math.random() * colors.length)]
-    
     heading.style.backgroundColor = color;
-
 });
 
 increaseFontBtn.addEventListener("click", ()=>{
-    
     let current_p_size = parseFloat(getComputedStyle(paragraph).fontSize);
     paragraph.style.fontSize = (current_p_size + 1) + 'px';
 });
 
-decreaseFontBtn.addEventListener("click", ()=>{
-    
+decreaseFontBtn.addEventListener("click", ()=>{ 
     let current_p_size = parseFloat(getComputedStyle(paragraph).fontSize);
     paragraph.style.fontSize = (current_p_size - 1) + 'px';
 });
 
 document.querySelector("html").addEventListener("mousemove", e => {
-//console.log(`mousemove x: ${e.clientX}, y: ${e.clientY}`)
 
-paragraphMouse.innerText = `mousemove x: ${e.clientX}, y: ${e.clientY}`;
-
+    paragraphMouse.innerText = `mousemove x: ${e.clientX}, y: ${e.clientY}`;
 });
 
 
