0b71d41b87c3d42cf78bbaa727e8cace80376ece
Suxumuxu <emmanouil.xylakis@gmail.com>
2026-06-06 09:51:06 +0200

    canvas2

--- JS_Files/2_compound_data.js
+++ JS_Files/2_compound_data.js
@@ -1,6 +1,5 @@
 // ------- JavaScript Lists -------
 
-
 let zygoi = [0,2,4,6,8]
 let monoi = [1,3,5,7,9]
 let unsorted = [5,2,9,1,5,6]
@@ -68,8 +67,6 @@ let casablanca = {
 "main_protagonist" : "Mathew Perry",
 "crew" : ['peter', 'georgia', 'helen']
 }
-
-
 
 //accessing object value using dot notation
 // console.log(casablanca.title)
--- JS_Files/4_eventHandlers.js
+++ JS_Files/4_eventHandlers.js
@@ -2,48 +2,60 @@ const clrbtn = document.getElementById("clrBtn");
 const increaseFontBtn = document.getElementById("increaseFontBtn");
 const decreaseFontBtn = document.getElementById("decreaseFontBtn");
 let paragraphMouse = document.getElementById("paragraph-mouse");
+let wrongBtn = document.querySelector(".wrong");
+let rightBtn = document.querySelector(".correct");
+let ansPara = document.getElementById("answerParagraph");
+const heading = document.getElementById("top-heading");
 
-heading.addEventListener("click", () => {
-console.log("You clicked the heading!");
-});
+// heading.addEventListener("click", () => {
+// console.log("You clicked the heading!");
+// });
 
-headings.forEach((heading) => {
-    heading.addEventListener("click", () => {
-        console.log("You clicked the heading!");
-    });
-});
+// headings.forEach((heading) => {
+//     heading.addEventListener("click", () => {
+//         console.log("You clicked the heading!");
+//     });
+// });
 
-clrbtn.addEventListener("click", ()=>{
+wrongBtn.addEventListener("click",()=>{
+    ansPara.innerText = "Wrong answer";
+})
+
+rightBtn.addEventListener("click",()=>{
+    ansPara.innerText = "Right Answer";
+})
+
+// clrbtn.addEventListener("click", ()=>{
     
-    const colors = ["blue","red","pink","yellow","green","cyan"];
-    const color = colors[Math.floor(Math.random() * colors.length)]
-    heading.style.backgroundColor = color;
-});
+//     const colors = ["blue","red","pink","yellow","green","cyan"];
+//     const color = colors[Math.floor(Math.random() * colors.length)]
+//     heading.style.backgroundColor = color;
+// });
 
-increaseFontBtn.addEventListener("click", ()=>{
-    let current_p_size = parseFloat(getComputedStyle(paragraph).fontSize);
-    paragraph.style.fontSize = (current_p_size + 1) + 'px';
-});
+// increaseFontBtn.addEventListener("click", ()=>{
+//     let current_p_size = parseFloat(getComputedStyle(paragraph).fontSize);
+//     paragraph.style.fontSize = (current_p_size + 1) + 'px';
+// });
 
-decreaseFontBtn.addEventListener("click", ()=>{ 
-    let current_p_size = parseFloat(getComputedStyle(paragraph).fontSize);
-    paragraph.style.fontSize = (current_p_size - 1) + 'px';
-});
+// decreaseFontBtn.addEventListener("click", ()=>{ 
+//     let current_p_size = parseFloat(getComputedStyle(paragraph).fontSize);
+//     paragraph.style.fontSize = (current_p_size - 1) + 'px';
+// });
 
-document.querySelector("html").addEventListener("mousemove", e => {
+// document.querySelector("html").addEventListener("mousemove", e => {
 
-    paragraphMouse.innerText = `mousemove x: ${e.clientX}, y: ${e.clientY}`;
-});
+//     paragraphMouse.innerText = `mousemove x: ${e.clientX}, y: ${e.clientY}`;
+// });
 
-function toggleBrightDarkMode(){
+// function toggleBrightDarkMode(){
     
-    document.documentElement.classList.toggle('mode-dark');
+//     document.documentElement.classList.toggle('mode-dark');
 
-    const isDark = document.documentElement.classList.contains('mode-dark');
-    document.getElementById('icon-sun').style.display  = isDark ? 'block'  : 'none';
-    document.getElementById('icon-moon').style.display = isDark ? 'none' : 'block';
+//     const isDark = document.documentElement.classList.contains('mode-dark');
+//     document.getElementById('icon-sun').style.display  = isDark ? 'block'  : 'none';
+//     document.getElementById('icon-moon').style.display = isDark ? 'none' : 'block';
 
-}
+// }
 
 
 
